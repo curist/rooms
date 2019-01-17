39501bbf18cc87d29ab030eff70099d32e56cda0
curist <curist.cyc@gmail.com>
2019-01-17 12:03:18 +0800

    when creating room, also create their module deps

--- src/room-modules/types.ts
+++ src/room-modules/types.ts
@@ -6,21 +6,22 @@ export enum RoomModuleType {
   Avalon = 'avalon',
 }
 
-export interface RoomModuleContext {
-  userId: number;
-  context?: {
-    [key in RoomModuleType]?: any;
-  }
-}
-
 export interface RoomReducerModule {
   defaultState: object;
   reducer: (state: object, action: object, context: RoomModuleContext) => object;
+  dependencies?: RoomModuleType[];
   validate?: (state: object, action: object) => null | Error;
 }
 
 export type RoomModules = {
   [key in RoomModuleType]: RoomReducerModule;
+}
+
+export interface RoomModuleContext {
+  userId: number;
+  context?: {
+    [key in RoomModuleType]?: any;
+  }
 }
 
 registerEnumType(RoomModuleType, {
